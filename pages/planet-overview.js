3b4d399c000a098373e298aa923871507cf23b1a
Zirdman <zirdman@gmail.com>
2023-12-10 19:07:09 +0200

    added a lot of HTML DOM elements

--- pages/planet-overview.js
+++ pages/planet-overview.js
@@ -1,5 +1,22 @@
+import { materials, materialGains, woodIncrease } from "../data/data.js";
+
 const backButton = document.getElementById("back-button");
 const loadingScreen = document.getElementById("loading-screen");
+const woodCount = document.getElementById("resource-1-p");
+
+const upgrade = {
+    first: {
+        location: document.getElementById("upgrade1"),
+        infoText: document.getElementById("upgrade1-info-text"),
+        statText: document.getElementById("upgrade1-stat-text"),
+    }
+};
+
+setInterval(function() {
+    woodCount.textContent = materials.wood;
+}, 1000);
+
+setInterval(woodIncrease, 1000);
 
 backButton.onclick = function returnToMenu() {
     loadingScreen.style.animation = "loadingScreen 1.5s forwards ease-in-out";
@@ -7,4 +24,8 @@ backButton.onclick = function returnToMenu() {
     setTimeout(function() {
         window.location.href = "../index.html";
     }, 1500);
+}
+
+upgrade.first.location.onclick = function firstUpgradeClicked() {
+    materialGains.wood++;
 }
