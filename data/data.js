4c6ff6d450cb5f82163a682fb3da4adc8f44cddf
Zirdman <zirdman@gmail.com>
2023-12-10 23:26:48 +0200

    removed redundant code

--- data/data.js
+++ data/data.js
@@ -1,10 +1,1 @@
-export let materials = {
-    wood: 0
-}
-export let materialGains = {
-    wood: 0
-}
-
-export function woodIncrease() {
-    materials.wood = materials.wood + materialGains.wood
-}
+// coins her elater ig
